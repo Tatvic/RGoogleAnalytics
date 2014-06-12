9480c0e92a0e3af5f66924d09fab2a4027002ddc
Kushan <kushan@tatvic.com>
2014-06-12 20:31:01 +0530

    Fixed the GenerateAccessToken() function call in ValidateToken() function

--- R/ValidateToken.R
+++ R/ValidateToken.R
@@ -1,6 +1,6 @@
 #' This function checks whether the Access Token stored in the local file is 
 #' expired. If yes, it generates a new Access Token and updates the local file.
-#' If no, then it returns the stored Access Token
+#' 
 #'  
 #' @export
 #'  
@@ -21,7 +21,10 @@ ValidateToken <- function() {
   if (check.token.param[1] != -1) {
     # If token has expired Generate a New Access token
     cat("Access Token had expired. Regenerating access token\n") 
-    GenerateAccessToken()
+    GenerateAccessToken(internal.file.path)
     return(invisible())
-  }    
+  } else {
+    cat("Current Access Token is valid")
+    return(invisible())
+  }   
 }
