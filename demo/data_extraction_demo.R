7a354f578997a82ceddba0c5fbef67ffdb49f8fa
Kushan <kushan@tatvic.com>
2014-06-16 22:55:17 +0530

    Updated Demo code

--- demo/data_extraction_demo.R
+++ demo/data_extraction_demo.R
@@ -1,6 +1,7 @@
 # Ensure you have copied the file client_secrets.json to the correct location
 # Authorize the Google Analytics account
-Auth()
+token <- Auth("150487456763-263oc9mh8q1427c788fl6piahlbcuqm3.apps.googleusercontent.com",
+              "TUmTHIdHFyHoeDhFad_TknUI")
 
 # Build a list of all the Query Parameters
 query.list <- Init(start.date = "2013-11-28",
@@ -10,8 +11,10 @@ query.list <- Init(start.date = "2013-11-28",
                    max.results = 1000,
                    table.id = "ga:33093633")
 
+profiles.df <- GetProfiles(token)
 # Create the Query Builder object so that the query parameters are validated
 ga.query <- QueryBuilder(query.list)
 
+ToUri(ga.query,token)
 # Extract the data and store it in a data-frame
-ga.data <- GetReportData(ga.query)
+ga.data <- GetReportData(ga.query,token,split_daywise=T)
