69759c4f8dcb80462368f606efc223829a6cc9e8
Kushan <kushan@tatvic.com>
2014-08-16 18:23:46 +0530

    Modified Date in description file

--- demo/data_extraction_demo.R
+++ demo/data_extraction_demo.R
@@ -3,8 +3,7 @@ require(RGoogleAnalytics)
 # Authorize the Google Analytics account
 # This need not be executed in every session once the token object is created 
 # and saved
-token <- Auth("150487456763-263oc9mh8q1427c788fl6piahlbcuqm3.apps.googleusercontent.com",
-              "TUmTHIdHFyHoeDhFad_TknUI")
+token <- Auth(client.id,client.secret)
 
 # Save the token object for future sessions
 save(token,file="./token_file")
@@ -19,6 +18,7 @@ query.list <- Init(start.date = "2013-11-28",
                    dimensions = "ga:date,ga:pagePath,ga:hour,ga:medium",
                    metrics = "ga:sessions,ga:pageviews",
                    max.results = 10000,
+                   sort = "-ga:date",
                    filters = "ga:medium==referral",
                    table.id = "ga:33093633")
 
@@ -26,4 +26,4 @@ query.list <- Init(start.date = "2013-11-28",
 ga.query <- QueryBuilder(query.list)
 
 # Extract the data and store it in a data-frame
-ga.data <- GetReportData(ga.query, token, split_daywise = T)
+ga.data <- GetReportData(ga.query, token)
